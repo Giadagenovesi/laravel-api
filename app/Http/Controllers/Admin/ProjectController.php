f7f06bc5708323f8249a6883d1f490e7bd9dc154
Giadagenovesi <giada.g@outlook.it>
2023-06-07 17:18:44 +0200

    Add select in create.php and modifyed show and index

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -5,9 +5,10 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreProjectRequest;
 use App\Http\Requests\UpdateProjectRequest;
+use App\Models\Type;
 use App\Models\Project;
-use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ProjectController extends Controller
 {
@@ -29,7 +30,8 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        return view('admin.projects.create');
+        $types = Type::all();
+        return view('admin.projects.create', compact('types'));
     }
 
     /**
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,8 +3,13 @@
 
 @section('content')
     <h1 class="text-center">{{ $project->title }}</h1>
-    <div class="text-end">
-        {{ $project->slug }}
+    <div class="d-flex justify-content-between">
+        @if ($project->type)
+            <span>Categoria: {{ $project->type->name }}</span>
+        @else
+            <span>Nessuna categoria</span>
+        @endif
+        <span>{{ $project->slug }}</span>
     </div>
     <p class="mt-4">{{ $project->content }}</p>
 @endsection
