01e96febff8b200062246316868c92fd45fd7c45
Giadagenovesi <giada.g@outlook.it>
2023-06-07 18:01:06 +0200

    implemented UPDATE CRUD

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -12,6 +12,18 @@
             <label for="title" class="form-label">Titolo</label>
             <input type="text" class="form-control" id="title" name="title" value="{{ old('title', $project->title) }}">
         </div>
+
+        <div class="mb-3">
+            <label for="type">Tipologia</label>
+            <select class="form-select" id="type" name="type_id">
+                <option value=""></option>
+                @foreach ($types as $type)
+                    <option @selected($type->id == old('type_id',$project->type?->id)) value="{{ $type->id }}">{{ $type->name }}</option>
+                @endforeach
+            </select>
+        </div>
+
+
         <div class="mb-3">
             <label for="content" class="form-label">Contentuto</label>
             <textarea class="form-control" id="content" rows="3" name="content">{{ old('content', $project->content) }}</textarea>
